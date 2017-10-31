cd77a8f07797b9d197cc726a13b202bbfad52b66
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2017-10-31 17:23:34 +0000

    Update socket-1.py

--- socket-1.py
+++ socket-1.py
@@ -12,11 +12,11 @@ period = 15     # Time (in seconds) between checks. Default is 15 seconds.
 
 def lights_on():
   os.system('curl -s "http://username:password@PI-IP-ADD-RESS/api-socket1-on/" > /dev/null')
-  print('Lights on! Room luminence is {0:.0f} lux.'.format(light.light()))
+  print('Socket 1 on! Room luminence is {0:.0f} lux.'.format(light.light()))
 
 def lights_off():
   os.system('curl -s "http://username:password@PI-IP-ADD-RESS/api-socet1-off/" > /dev/null')
-  print('Lights off! Room luminence is {0:.0f} lux.'.format(light.light()))
+  print('Socket 1 off! Room luminence is {0:.0f} lux.'.format(light.light()))
 
 lux = 250       # Defines when the lights will come on or off.
 
