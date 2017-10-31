2deecb00b52f4550046c13f55257e8398fb4d5f7
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2017-10-31 17:09:48 +0000

    Update light-sense.py
    Amended code to print lux levels in terminal and ensured the lux level variable is moved to the appropriate place.

--- testing/light-sense.py
+++ testing/light-sense.py
@@ -6,23 +6,25 @@ import time
 from envirophat import light
 
 # Variables used defined below
-lux = 250        # Light level to trigger action
-state = 0        # Logic to tell whether the light is on or off
-period = 0.10    # Time (in seconds) between checks. Default is 0.10.
+
+state = 0       # Logic to tell whether the light is on or off.
+period = 15     # Time (in seconds) between checks. Default is 15 seconds.
 
 def lights_on():
   os.system('curl -s "http://username:password@PI-IP-ADD-RESS/api-all-on/" > /dev/null')
-  print('Lights on!')
-  
+  print('Lights on! Room luminence is {0:.0f} lux.'.format(light.light()))
+
 def lights_off():
   os.system('curl -s "http://username:password@PI-IP-ADD-RESS/api-all-off/" > /dev/null')
-  print('Lights off!')
+  print('Lights off! Room luminence is {0:.0f} lux.'.format(light.light()))
+
+lux = 250       # Defines when the lights will come on or off.
 
 while True:
   if light.light() < lux and state != 1:
-      lights_on
+      lights_on()
       state = 1
   elif light.light() >= lux and state == 1:
-      lights_off
+      lights_off()
       state = 0
   time.sleep(period)
