0a8ce356e91ede65239af05b963f968c90d14ca6
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2017-10-31 17:24:58 +0000

    Create socket-2.py

---
+++ socket-2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Import the necessary modules, including the Enviro pHAT module
+import os
+import time
+from envirophat import light
+
+# Variables used defined below
+
+state = 0       # Logic to tell whether the light is on or off.
+period = 15     # Time (in seconds) between checks. Default is 15 seconds.
+
+def lights_on():
+  os.system('curl -s "http://username:password@PI-IP-ADD-RESS/api-socket2-on/" > /dev/null')
+  print('Socket 2 on! Room luminence is {0:.0f} lux.'.format(light.light()))
+
+def lights_off():
+  os.system('curl -s "http://username:password@PI-IP-ADD-RESS/api-socet2-off/" > /dev/null')
+  print('Socket 2 off! Room luminence is {0:.0f} lux.'.format(light.light()))
+
+lux = 250       # Defines when the lights will come on or off.
+
+while True:
+  if light.light() < lux and state != 1:
+      lights_on()
+      state = 1
+  elif light.light() >= lux and state == 1:
+      lights_off()
+      state = 0
+  time.sleep(period)
